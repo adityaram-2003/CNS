4d272a62e5224db7491b5dfea7f045275ab1f41d
Adityaram Komaraneni <104423060+adityaram-2003@users.noreply.github.com>
2023-07-10 16:18:51 +0530

    Update task_05b.c

--- task_05b.c
+++ task_05b.c
@@ -3,14 +3,9 @@
 
 int main()
 {
-    // Read the input string
     char plain[100] = "conrodlemar";
     printf("Before stuffing: %s\n", plain);
-
-    // Define the start flag, esc flag and end flag
     char start[100] = "dlestx", esc[100] = "dle", end[100] = "dleetx";
-
-    // Escape the escape character
     int pl = strlen(plain), el = strlen(esc);
     int i = 0, j = 0;
     char copy[100] = {0};
@@ -25,13 +20,10 @@ int main()
         }
         strncat(copy, plain + i, 1);
     }
-
-    // Add stuffing to frame
     char frame[100] = {0};
     strcat(frame, start);
     strcat(frame, copy);
     strcat(frame, end);
     printf("After stuffing: %s\n", frame);
-
     return 0;
 }
